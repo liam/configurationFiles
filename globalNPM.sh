07f02abc6ebe4526fc3192d24b83933834021535
Si <simondrake1990@hotmail.co.uk>
2018-08-21 08:20:46 +0100

    Create globalNPM.sh

---
+++ globalNPM.sh
@@ -0,0 +1,18 @@
+#####################
+# general utilities #
+#####################
+
+npm install -g tldr
+npm install -g spoof
+npm install -g fkill-cli
+npm install -g vtop
+
+npm install -g local-npm
+npm install -g nodemon
+npm install -g pm2
+
+#####################
+#      react        #
+#####################
+npm install -g create-react-app
+
