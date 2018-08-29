e834ad614327a10f8f0d159ba06877e4b1e7a40e
Simon Drake <sdrake@theworkshop.com>
2018-08-29 08:53:41 +0100

    brew installs

---
+++ brewInstalls.sh
@@ -0,0 +1,26 @@
+###########################
+#	Initial Setup
+############################
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+###########################
+#       Brew Installs
+############################
+
+brew install csshx
+brew install git
+brew install postgresql
+brew install telnet
+brew install zsh
+brew install zsh-completions
+brew install z
+
+
+###########################
+#       Brew Cask Installs
+############################
+
+brew cask install dbeaver-community
+brew cask install font-fira-code
+brew cask install iterm2
